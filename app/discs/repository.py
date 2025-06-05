20e5f562f6a13db6fc09c6f6253417d0b74b957c
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-06-05 00:43:12 -0400

    Added sort by author

--- app/discs/repository.py
+++ app/discs/repository.py
@@ -2,7 +2,7 @@ from app.db import get_db
 
 def find_all():
     db = get_db()
-    discs = list(db['discs'].find({'enabled': True}, {'_id': 0})).sort('author', 1)
+    discs = list(db['discs'].find({'enabled': True}, {'_id': 0}).sort('author', 1))
 
     return discs
 
@@ -14,12 +14,11 @@ def find_by_sku(sku: str):
 
 def find_by_type(type: str):
     db = get_db()
-    discs = list(db['discs'].find({'type': type, 'enabled': True}, {'_id': 0})).sort('author', 1)
-
+    discs = list(db['discs'].find({'type': type, 'enabled': True}, {'_id': 0}).sort('author', 1))
     return discs
 
 def find_favorite():
     db = get_db()
-    discs = list(db['discs'].find({'favorite': True, 'enabled': True}, {'_id': 0})).sort('author', 1)
+    discs = list(db['discs'].find({'favorite': True, 'enabled': True}, {'_id': 0}).sort('author', 1))
 
     return discs
